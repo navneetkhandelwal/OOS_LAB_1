be46119dce05c98e9b53c99595fd717480c98936
Navneet Khandelwal <69151973+navneetkhandelwal@users.noreply.github.com>
2021-06-03 21:05:50 +0530

    c file ossd lab assignment 1

---
+++ main.c
@@ -0,0 +1,40 @@
+//
+//  main.c
+//  OOS_LAB_1
+//
+//  Created by Shweta Khandelwal on 03/06/21.
+//
+
+# include <stdio.h>
+# include <string.h>
+   
+int main( )
+{
+    FILE *filePointer ;
+    char dataToBeWritten[50]
+        = "GeeksforGeeks-A Computer Science Portal for Geeks";
+  
+    filePointer = fopen("GfgTest.c", "w") ;
+    
+    if ( filePointer == NULL )
+    {
+        printf( "GfgTest.c file failed to open." ) ;
+    }
+    else
+    {
+          
+        printf("The file is now opened.\n") ;
+        if ( strlen (  dataToBeWritten  ) > 0 )
+        {
+            fputs(dataToBeWritten, filePointer) ;
+            fputs("\n", filePointer) ;
+        }
+
+        fclose(filePointer) ;
+          
+        printf("Data successfully written in file GfgTest.c\n");
+        printf("The file is now closed.") ;
+    }
+    return 0;
+}
+
